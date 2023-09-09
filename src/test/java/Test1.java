4575281d6abe49c7284da0a1cd4fe6ff50e02015
sergud14 <sergud@bk.ru>
2023-09-09 21:00:58 +0300

    Test1

---
+++ src/test/java/Test1.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Test1 {
+    @Test
+    public void test1(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
