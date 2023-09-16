c6589db4641ca0b0623e3eb62f1781c55c8ab89c
sergud14 <sergud@bk.ru>
2023-09-16 15:32:30 +0300

    TestLongRedirect

---
+++ src/test/java/TestLongRedirect.java
@@ -0,0 +1,32 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+public class TestLongRedirect{
+    @Test
+    public void testLongRedirection(){
+
+        int statusCode;
+        int redirectCount=0;
+        String url="https://playground.learnqa.ru/api/long_redirect";
+
+        do{
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)
+                    .when()
+                    .get(url)
+                    .andReturn();
+
+            url = response.getHeader("Location");
+            statusCode = response.getStatusCode();
+
+            if(statusCode==301) {
+                redirectCount++;
+                System.out.println(url);
+            }
+        }
+        while(statusCode!=200);
+        System.out.println("Кол-во редиректов:"+ redirectCount);
+    }
+}
