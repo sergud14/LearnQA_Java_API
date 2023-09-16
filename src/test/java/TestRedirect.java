3487479c7c1243fc981c45e2d7c773e8a703bfe8
sergud14 <sergud@bk.ru>
2023-09-16 15:11:51 +0300

    TestRedirect_update

--- src/test/java/TestRedirect.java
+++ src/test/java/TestRedirect.java
@@ -1,18 +1,20 @@
-
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 public class TestRedirect {
     @Test
     public void testRedirection(){
+
         Response response  = RestAssured
                 .given()
                 .redirects()
-                .follow(true)
+                .follow(false)
+                .expect().statusCode(301)
+                .when()
                 .get("https://playground.learnqa.ru/api/long_redirect")
                 .andReturn();
 
-        String redirectUrl = response.getHeader("X-Host");
+        String redirectUrl = response.getHeader("Location");
         System.out.println(redirectUrl);
     }
 }
