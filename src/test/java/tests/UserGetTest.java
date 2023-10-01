68029196efc86ee7121c3e4bf289a8ad120a51ab
sergud14 <sergud@bk.ru>
2023-10-01 11:56:52 +0300

    Ex16

--- src/test/java/tests/UserGetTest.java
+++ src/test/java/tests/UserGetTest.java
@@ -1,6 +1,7 @@
 package tests;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import lib.ApiCoreRequests;
 import lib.BaseTestCase;
 import org.junit.jupiter.api.Test;
 import java.util.HashMap;
@@ -8,6 +9,8 @@ import java.util.Map;;
 import lib.Assertions;
 
 public class UserGetTest extends BaseTestCase {
+
+    private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
     @Test
     public void testGetUserDataNotAuth() {
         Response repsonseUserData = RestAssured
@@ -46,4 +49,26 @@ public class UserGetTest extends BaseTestCase {
         String [] expectedFields = {"username","firstName","lastName","email"};
         Assertions.assertJsonHasFields(responseUserData,expectedFields);
     }
+
+    @Test
+    public void testGetUserDetailsAuthAsOtherUser() {
+        Map<String,String> authData= new HashMap<>();
+        authData.put("email","vinkotov@example.com");
+        authData.put("password", "1234");
+
+        Response responseGetAuth = apiCoreRequests
+                .makePostRequest("https://playground.learnqa.ru/api/user/login",authData);
+
+        String header = this.getHeader(responseGetAuth,"x-csrf-token");
+        String cookie = this.getCookie(responseGetAuth,"auth_sid");
+
+        Response responseUserData = apiCoreRequests
+                .makeGetRequest("https://playground.learnqa.ru/api/user/1",header,cookie);
+
+        Assertions.assertJsonHasKey(responseUserData, "username");
+        Assertions.assertJsonHasNotKey(responseUserData, "firstName");
+        Assertions.assertJsonHasNotKey(responseUserData, "lastName");
+        Assertions.assertJsonHasNotKey(responseUserData, "email");
+    }
+
 }
